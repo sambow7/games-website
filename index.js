11c4852aea2432bade11bec7e85146ab0f3d53be
Sam Bowen <samhbowen@gmail.com>
2023-03-12 13:35:55 -0700

    adjusting grammar

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 alert("A name must be given to enter");
 let usersName = prompt("A name must be given to enter");
 
-console.log("Hi " +usersName + ",hope you like games?");
-document.write("Hi " +usersName + ",hope you like games?")
+console.log("Hi " +usersName + ", hope you like games?");
+document.write("Hi " +usersName + ", hope you like games?")
 
 if (usersName == "Kassie") {
   alert("Ello guvna! Thanks for checking on me teach.");
