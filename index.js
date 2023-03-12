91e0168898308bc14c99fdea0ac2deeb2afade5f
Sam Bowen <samhbowen@gmail.com>
2023-03-11 18:05:38 -0800

    checking java function

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ let usersName = prompt("A name must be given to enter");
 
 console.log("Hi " +usersName + " welcome to my page!");
 
-document.write("Hi " +usersName + " welcometo my page!");
+document.write("Hi " +usersName + " welcome to my page!");
+
+if (usersName == "Kassie"){
+  alert("Ello govna! Thanks for checking on me teach.");
+} else
+  alert()
+
