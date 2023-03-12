0f406d1e997e826c5a2bbbbbf708e0fb64a10f9f
Sam Bowen <samhbowen@gmail.com>
2023-03-11 17:24:12 -0800

    checking java function

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-let usersName = prompt("what is your name?");
+let usersName = prompt("A name must be given to enter");
 
 console.log("Hi" + usersName + "welcome to my page!");
 
 document.write("Hi" + usersName + "welcometo my page!");
+
+if (theirName == "Kassie") {
+  alert("It's a match! Hi teach thanks for checking out my site");
+  else
+  alert("Congrats, you have arrived")
+}
