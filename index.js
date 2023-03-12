ea1ab583555f6fd16fd7825a5b8064e0fbf5790e
Sam Bowen <samhbowen@gmail.com>
2023-03-11 18:20:56 -0800

    checking java function

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let usersName = prompt("A name must be given to enter");
 
-console.log("Hi " +usersName + " welcome to my page!");
+console.log("Hi " +usersName + ",hope you like games?");
 
-document.write("Hi " +usersName + " welcome to my page!");
-
-if (usersName == "Kassie"){
-  alert("Ello govna! Thanks for checking on me teach.");
-} else
-  alert()
-
+if (usersName == "Kassie") {
+  alert("Ello guvna! Thanks for checking on me teach.");
+} else if (usersName == "Sam") {
+  alert("Ello Master");
+} else if (usersName == "potato") {
+  alert("MMMMMM po-ta-to")
+}
